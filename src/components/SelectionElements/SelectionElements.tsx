3d06f379e20e53018978f2449419d23214d09869
Jhon Garcia <metalprime57@gmail.com>
2021-03-16 14:36:21 -0500

    creation DisplayImg component

--- src/components/SelectionElements/SelectionElements.tsx
+++ src/components/SelectionElements/SelectionElements.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './SelectionElements.css';
 
 export const SelectionElements =  () => {
 
     return (
-        <div>
+        <div className = 'SelectionElements'>
             <p>Lorem ipsum dolor dandole,puto el q lo lea sit amet consectetur adipisicing elit. Corporis et nulla, vel quod quas ad expedita.</p>
             <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolore rem quibusda</h1>
         </div>
