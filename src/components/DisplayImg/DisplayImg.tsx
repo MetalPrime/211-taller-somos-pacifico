49f1418d22459a1b15688f169bf053172d86b1ab
Jhon Garcia <metalprime57@gmail.com>
2021-03-16 23:19:18 -0500

    basic css organization

--- src/components/DisplayImg/DisplayImg.tsx
+++ src/components/DisplayImg/DisplayImg.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import './DisplayImg.css';
 
 export const DisplayImg = () => {
-    
+    return(
+        <div className='DisplayImg'>
+            <img src="" alt=""/>
+        </div>
+    );
 }
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import './App.css';
+import { DisplayImg } from '../../components/DisplayImg/DisplayImg';
 import { SelectionElements } from '../../components/SelectionElements/SelectionElements';
 
 export const App = () => {
     return (
-        <main>
-            <SelectionElements></SelectionElements>
-            <SelectionElements />
-            <SelectionElements />
+        <main className='App'>
+            <article className='App__commands'>
+                <section>
+                    <SelectionElements></SelectionElements>
+                    <SelectionElements />
+                    <SelectionElements />
+                </section>
+                <section></section>
+                <section></section>
+            </article>
+            <article className='App__display'>
+            <DisplayImg></DisplayImg>
+            </article>
+
         </main>
     );
 }
