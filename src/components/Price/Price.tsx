50342eaa1c7c6779bdc570bb4f9ed156189e8c6b
Jhon Garcia <metalprime57@gmail.com>
2021-03-27 18:41:50 -0500

    Update Price.tsx

--- src/components/Price/Price.tsx
+++ src/components/Price/Price.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './Price.css';
 
 interface Price {
-    currentPrice: number,
+    currentPrice?: number,
 }
 
 export const Price: React.FC<Price> = ({ currentPrice }) => {
