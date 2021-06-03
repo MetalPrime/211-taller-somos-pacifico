752657500d7daedcc674be2c63d8f311842de9c4
Jhon Garcia <metalprime57@gmail.com>
2021-06-03 01:01:22 -0500

    visualizacion de Buy

--- src/components/Buy/Buy.tsx
+++ src/components/Buy/Buy.tsx
@@ -16,7 +16,7 @@ export const Buy: React.FC<Buy> = ({ products }) => {
     return (
         <section className='Buy'>
             <h1>Comprar</h1>
-            <Link to='/newProduct'>Crear nuevo elemento</Link> 
+            <Link to='/newProduct' className="Buy__new" >Crear nuevo elemento</Link> 
             <section className='Buy__Options'>
                 {
                     products.map(product => {
